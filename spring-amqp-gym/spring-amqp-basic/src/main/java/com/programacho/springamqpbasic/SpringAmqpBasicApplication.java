50cd4c7e13ea215dae8a3f869698722e502835f9
hainet50b <20400616+hainet50b@users.noreply.github.com>
2023-05-05 17:38:56 +0900

    init

---
+++ spring-amqp-gym/spring-amqp-basic/src/main/java/com/programacho/springamqpbasic/SpringAmqpBasicApplication.java
@@ -0,0 +1,13 @@
+package com.programacho.springamqpbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAmqpBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAmqpBasicApplication.class, args);
+    }
+
+}
---
+++ spring-amqp-gym/spring-amqp-basic/src/test/java/com/programacho/springamqpbasic/SpringAmqpBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programacho.springamqpbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAmqpBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
